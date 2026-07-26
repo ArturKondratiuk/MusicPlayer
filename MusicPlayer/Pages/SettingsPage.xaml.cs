3b97e6400f5d1a662e1847a9fc2fe6867517bd72
Artur Kondratiuk <g00446586@atu.ie>
2026-07-27 00:08:06 +0100

    added settings page; updated Setting.cs; added dark/light mode (working incorrectly on playlistPage - will fix it soon); added base sorting of track in LibraryPage in SettingsPage;

--- MusicPlayer/Pages/SettingsPage.xaml.cs
+++ MusicPlayer/Pages/SettingsPage.xaml.cs
@@ -1,9 +1,55 @@
+using MusicPlayer.Models;
+using MusicPlayer.Services;
+
 namespace MusicPlayer.Pages;
 
 public partial class SettingsPage : ContentPage
 {
-	public SettingsPage()
-	{
-		InitializeComponent();
-	}
+    private readonly SettingsService settingsService = new();
+
+    private Settings settings = new();
+
+    public SettingsPage()
+    {
+        InitializeComponent();
+
+        Loaded += async (_, _) =>
+        {
+            settings = await settingsService.LoadAsync();
+
+            DarkModeSwitch.IsToggled = settings.DarkMode;
+
+            SortPicker.SelectedItem = settings.DefaultSort;
+
+            ApplyTheme();
+        };
+    }
+
+    private void DarkModeSwitch_Toggled(object sender, ToggledEventArgs e)
+    {
+        settings.DarkMode = e.Value;
+
+        ApplyTheme();
+    }
+
+    private void ApplyTheme()
+    {
+        Application.Current!.UserAppTheme =
+            settings.DarkMode
+            ? AppTheme.Dark
+            : AppTheme.Light;
+    }
+
+    private async void Save_Clicked(object sender, EventArgs e)
+    {
+        settings.DefaultSort =
+            SortPicker.SelectedItem?.ToString() ?? "Title";
+
+        await settingsService.SaveAsync(settings);
+
+        await DisplayAlert(
+            "Settings",
+            "Settings saved.",
+            "OK");
+    }
 }
--- MusicPlayer/Services/SettingsService.cs
+++ MusicPlayer/Services/SettingsService.cs
@@ -1,14 +1,28 @@
-﻿using System;
+﻿using MusicPlayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MusicPlayer.Models;
 
-namespace MusicPlayer.Services
+namespace MusicPlayer.Services;
+
+public class SettingsService
 {
-    internal class SettingsService
+    private readonly JsonService jsonService = new();
+
+    private readonly string filePath =
+        Path.Combine(FileSystem.AppDataDirectory, "settings.json");
+
+    public async Task<Settings> LoadAsync()
     {
-        public Settings Settings { get; } = new();
+        var settings = await jsonService.LoadAsync<Settings>(filePath);
+
+        return settings ?? new Settings();
+    }
+
+    public async Task SaveAsync(    Settings settings)
+    {
+        await jsonService.SaveAsync(filePath, settings);
     }
 }
